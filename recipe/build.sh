4bf829bfeb6f56ebf374031fdf09a68bbfb45348
Zhe Wang <0x1998@gmail.com>
2018-09-18 18:04:23 +0800

    Update to 0.6.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ set -e -x
 
 mkdir -p build && cd build
 cmake -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DYAML_CPP_BUILD_TOOLS=off \
     -DBUILD_SHARED_LIBS=on \
